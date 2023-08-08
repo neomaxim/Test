fb6ca35e2f4dd5ab865c04197b78afe98aa35734
neomaxim <neomaximys@gmail.com>
2023-08-08 15:03:47 +0300

    new lesson2,hw2

---
+++ src/lesson2/hw2/Main.java
@@ -0,0 +1,13 @@
+package lesson2.hw2;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int i = 0;
+        while(i <= 25){
+            System.out.print(i + " ");
+            i++;
+        }
+
+    }
+}
